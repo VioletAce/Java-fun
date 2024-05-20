243b2eaa8c2350f1fd4ad8c3ca002683f2a208b5
VioletAce <violetace98@gmail.com>
2024-05-20 17:48:14 -0500

    Beginner script, hello world lol

---
+++ Quickstart.java
@@ -0,0 +1,10 @@
+// JoEnid Acevedo
+// First program!
+// Yay doing stuff!
+
+class QuickStart {
+    public static void main (String[] args) {
+        System.out.println("Hello, World.");
+        System.out.print("Hello World but simpler");
+    }
+}
